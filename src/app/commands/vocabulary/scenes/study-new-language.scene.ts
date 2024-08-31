c821f8312d12bc7f8cfa3e5a1688c4205458fddd
Xanday <xandaychannel@gmail.com>
2024-08-31 22:44:05 +0300

    transferred flashcard-property to json-property

--- src/app/commands/vocabulary/scenes/study-new-language.scene.ts
+++ src/app/commands/vocabulary/scenes/study-new-language.scene.ts
@@ -1,17 +1,15 @@
+import { Apply, CreateScene } from '../../../../core';
 import { TelegramContext } from '../../../../core/ctx.class';
 import { ModifyParams } from '../../../../core/decorators/modify-params/modify-params.decorator';
+import { CreateSelectButtonComposer } from '../../../../core/decorators/scene/composers';
 import { Scene } from '../../../../core/decorators/scene/types';
 import { InterfaceLanguages } from '../../../../core/language-interface/enums';
-import { CreateSelectButtonComposer } from '../../../../core/decorators/scene/composers';
 import { vocabularyService } from '../../../services/database/vocabulary/vocabulary.service';
-import { CreateFinishReplyAction } from '../../../shared/actions';
-import { SelectLanguageAction } from '../../../shared/actions';
+import { CreateFinishReplyAction, SelectLanguageAction } from '../../../shared/actions';
 import { VocabularyManaging } from '../../../shared/classes';
 import { LanguageJsonFormat } from '../../../shared/constants';
 import { IsNotLearningLanguageMiddleware } from '../../../shared/middlewares';
-import { GetVocabularyManaging } from '../../../shared/modify-params';
-import { TransformLanguage } from '../../../shared/modify-params';
-import { Apply, CreateScene } from '../../../../core';
+import { GetVocabularyManaging, TransformLanguage } from '../../../shared/modify-params';
 import { ApplyServicePartAction } from '../../../shared/part-actions/apply-service.part-action';
 
 @CreateScene('vocabulary-study-new-language-scene')
@@ -27,7 +25,7 @@ export class VocabularyStudyNewLanguageScene implements Scene {
     @Apply({middlewares: [IsNotLearningLanguageMiddleware], possibleErrors: []})
     @ModifyParams()
     async afterSelectStudyLanguage(ctx: TelegramContext, @TransformLanguage('language') language: InterfaceLanguages) {
-        await ApplyServicePartAction(ctx,vocabularyService, 'add', {}, {user: ctx.session['user'], language, flashcards: []});
+        await ApplyServicePartAction(ctx,vocabularyService, 'add', {}, {user: ctx.session['user'], language, json: []});
 
         CreateFinishReplyAction(ctx, 'VOCABULARY.STUDY_NEW_LANGUAGE.FINISHED', ctx.session['user'].interfaceLanguage);
     }
